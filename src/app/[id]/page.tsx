9cc3e0460900eab6b430d6928267ae355b42e077
carlos-dubon <holacarlosdubon@gmail.com>
2023-09-26 13:14:19 -0600

    gaps

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -4,15 +4,19 @@ import { db } from "@/app/firebase/config";
 import Link from "next/link";
 
 export interface User {
-  bgColor: string;
-  bgImage: string;
   imageUrl: string;
   name: string;
+  description: string;
   socialLinks: {
     slug: string;
     socialNetwork: string;
   }[];
-  iconPack: string;
+  layoutConfig: {
+    bgColor: string;
+    bgImage: string;
+    iconPack: string;
+    font: string;
+  };
 }
 
 export default async function Page({ params }: { params: { id: string } }) {
@@ -26,19 +30,21 @@ export default async function Page({ params }: { params: { id: string } }) {
   return (
     <div
       style={{
-        backgroundColor: user?.bgColor,
+        backgroundColor: user?.layoutConfig.bgColor,
       }}
-      className="text-white h-screen flex flex-col items-center p-6 gap-2"
+      className="text-white h-screen flex flex-col items-center p-6"
     >
       <img
         src={user?.imageUrl}
         alt={user?.name}
         width={96}
-        className="rounded-full"
+        className="rounded-full mb-4"
       />
 
       <p className="text-xl font-medium">{user?.name}</p>
-      <div className="flex gap-2">
+      <p className="text-sm mt-1">{user?.description}</p>
+
+      <div className="flex gap-2 mt-6">
         {user?.socialLinks.map((socialLink, index) => {
           return (
             <Link href={"#"} key={index}>
@@ -47,6 +53,9 @@ export default async function Page({ params }: { params: { id: string } }) {
           );
         })}
       </div>
+
+      {/* Custom links section */}
+      <div className="mt-8"></div>
     </div>
   );
 }
