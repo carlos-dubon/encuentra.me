cc4ac0dd65d4f66e096e3eb7b9d05f941cb4c026
Carlos Dubón <holacarlosdubon@gmail.com>
2023-11-06 13:09:15 -0600

    loaders

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 import { USE_GET_USER_ERROR, useGetUser } from "@/hooks/useGetUser";
+import { DotLoader } from "react-spinners";
 
-export default function Dashboard({ params }: { params: { uid: string } }) {
+export default function Dashboard() {
   const { loading, error, user } = useGetUser();
 
   if (loading) {
-    return "Cargando...";
+    return (
+      <div className="h-screen w-full flex flex-col justify-center items-center bg-gradient-to-b from-pink-300 to bg-pink-50">
+        <DotLoader color="#5C97FF" size={70} />
+        <div className="mt-4 text-lg font-medium">Cargando tu dashboard</div>
+      </div>
+    );
   }
 
   if (error == USE_GET_USER_ERROR.USER_NOT_FOUND) {
